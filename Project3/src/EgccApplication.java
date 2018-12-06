24aa9912ad940bef3dccd88ce7cb08965faf30d9
SprechND30 <38571612+SprechND30@users.noreply.github.com>
2018-12-06 00:07:51 -0500

    Main loop skeleton done
    Main loop skelly finished, just needs filling in for most functions

--- Project3/src/EgccApplication.java
+++ Project3/src/EgccApplication.java
@@ -20,25 +20,47 @@ public class EgccApplication {
 		
 		while(running){
 			//Get user input
-			System.out.println("Please enter one of the following commands:\n0: Change my password\n1: View items I bid on\n2: View my items\n3: View my purchases\n4: Search by keyword\n5: View seller rating\n6: View my purchases\n7: Put item up for auction\n8: Ship an item\n9: View highest bid\n10: Place a bid\n11: Rate a seller\n12: Close an auction\n13: Exit eGCC\nInput a command: ");
+			System.out.println("Please enter one of the following commands:\n0: Login\n1: Change my password\n2: View items I bid on\n3: View my items\n4: View my purchases\n5: Search by keyword\n6: View seller rating\n7: Put item up for auction\n8: Ship an item\n9: View highest bid\n10: Place a bid\n11: Rate a seller\n12: Close an auction\n13: Exit eGCC\nInput a command: ");
 			command = scnr.nextInt();
 			
 			//execute command
-			if(command == 13){
+			if(command == 13){					//exiting the application
 				running = false;
 				connect.closeConnection();
-			}else if(command == 0){
-				System.out.println("Please enter the user's username: ");
+			}else if(command == 0){				//login
+				System.out.println("Please enter your username: ");
 				userNm = scnr.next();
 				System.out.println("Password: ");
 				psswd = scnr.next();
 				connect.login(userNm, psswd);
-			}else if(command == 1){
+			}else if(command == 1){				//change password
 				System.out.println("Please enter your username: ");
 				userNm = scnr.next();
 				System.out.println("Please enter your new password");
 				psswd = scnr.next();
 				connect.changePassword(userNm, psswd);
+			}else if(command == 2){				//view items I bid on
+				//TODO: fill in
+			}else if(command == 3){				//view my items
+				//TODO: fill in
+			}else if(command == 4){				//view my purchases
+				//TODO: fill in
+			}else if(command == 5){				//search by keyword
+				//TODO: fill in
+			}else if(command == 6){				//view seller rating
+				//TODO: fill in
+			}else if(command == 7){				//put item up for auction
+				//TODO: fill in
+			}else if(command == 8){				//ship item
+				//TODO: fill in
+			}else if(command == 9){				//view highest bid
+				//TODO: fill in
+			}else if(command == 10){			//place bid
+				//TODO: fill in
+			}else if(command == 11){			//rate seller
+				//TODO: fill in
+			}else if(command == 12){			//close auction
+				//TODO: fill in
 			}
 		}
 		
